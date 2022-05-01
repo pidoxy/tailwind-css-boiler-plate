c133a82df2f8b5398c6fbe9918d8d612fcc37b1b
IDOKO EMMANUEL <pidox31102002@gmail.com>
2022-05-01 22:59:23 +0100

    href links made accessible

--- src/components/Header.js
+++ src/components/Header.js
@@ -47,7 +47,7 @@ const Header = () => {
             <ul className="py-1" aria-labelledby="dropdown">
               <li>
                 <a
-                  href="#"
+                  href="/"
                   className="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
                 >
                   Dashboard
@@ -55,7 +55,7 @@ const Header = () => {
               </li>
               <li>
                 <a
-                  href="#"
+                  href="/"
                   className="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
                 >
                   Settings
@@ -63,7 +63,7 @@ const Header = () => {
               </li>
               <li>
                 <a
-                  href="#"
+                  href="/"
                   className="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
                 >
                   Earnings
@@ -71,7 +71,7 @@ const Header = () => {
               </li>
               <li>
                 <a
-                  href="#"
+                  href="/"
                   className="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
                 >
                   Sign out
@@ -120,7 +120,7 @@ const Header = () => {
           <ul className="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
             <li>
               <a
-                href="#"
+                href="/"
                 className="block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white"
                 aria-current="page"
               >
