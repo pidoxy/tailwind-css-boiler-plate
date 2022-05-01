1f1b0398f3be4d0c6c33a7f2ea95e593b57ee1e9
IDOKO EMMANUEL <pidox31102002@gmail.com>
2022-05-01 23:02:03 +0100

    href links made accessible

--- src/components/Header.js
+++ src/components/Header.js
@@ -129,7 +129,7 @@ const Header = () => {
             </li>
             <li>
               <a
-                href="#"
+                href="/"
                 className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
               >
                 About
@@ -137,7 +137,7 @@ const Header = () => {
             </li>
             <li>
               <a
-                href="#"
+                href="/"
                 className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
               >
                 Services
@@ -145,7 +145,7 @@ const Header = () => {
             </li>
             <li>
               <a
-                href="#"
+                href="/"
                 className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
               >
                 Pricing
@@ -153,7 +153,7 @@ const Header = () => {
             </li>
             <li>
               <a
-                href="#"
+                href="/"
                 className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
               >
                 Contact
